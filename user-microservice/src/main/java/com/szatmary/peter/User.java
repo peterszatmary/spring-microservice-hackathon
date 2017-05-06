80aafadbab1eca96ed37a0fc355c825047791e04
Peter Szatmary <peter.szatmary@gmail.com>
2017-05-06 13:49:31 +0200

    USER: user microservice just as simple microservice that acts also as config client.

--- user-microservice/src/main/java/com/szatmary/peter/User.java
+++ user-microservice/src/main/java/com/szatmary/peter/User.java
@@ -1,7 +1,15 @@
 package com.szatmary.peter;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Created by peterszatmary on 06/05/2017.
  */
+@Data
+@AllArgsConstructor
 public class User {
+    private String name;
+    private String surname;
+    private String role;
 }
--- user-microservice/src/main/java/com/szatmary/peter/UserController.java
+++ user-microservice/src/main/java/com/szatmary/peter/UserController.java
@@ -1,7 +1,42 @@
 package com.szatmary.peter;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by peterszatmary on 06/05/2017.
  */
+@RestController
 public class UserController {
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${admin.surname}")
+    private String adminSurname;
+
+    @Value("${admin.role}")
+    private String adminRole;
+
+    @Value("${superuser.name}")
+    private String superUserName;
+
+    @Value("${superuser.surname}")
+    private String superUserSurname;
+
+    @Value("${superuser.role}")
+    private String superUserRole;
+
+    @RequestMapping("/users")
+    public List<User> showSystemUsers() {
+        List<User> result = new ArrayList<>();
+        result.add(new User(adminName, adminSurname, adminRole));
+        result.add(new User(superUserName, superUserSurname, superUserRole));
+        return result;
+    }
 }
