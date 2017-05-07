db3d693228d1a78fdef0878a76baa0242d07fdd0
Peter Szatmary <peter.szatmary@gmail.com>
2017-05-07 10:48:20 +0200

    CONF: aplication renamed to configuration server application.

--- configuration-server/src/main/java/com/szatmary/peter/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/com/szatmary/peter/ConfigurationServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class Application {
+public class ConfigurationServerApplication {
 
     public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(ConfigurationServerApplication.class, args);
   }
 }
