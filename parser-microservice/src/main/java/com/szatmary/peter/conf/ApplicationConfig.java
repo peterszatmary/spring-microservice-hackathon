c0c2bb9fb118127426c9b236874204bc1815703e
Peter Szatmary <peter.szatmary@codexa.sk>
2018-11-23 21:09:02 +0100

    Fixed some minor issues.

--- parser-microservice/src/main/java/com/szatmary/peter/conf/ApplicationConfig.java
+++ parser-microservice/src/main/java/com/szatmary/peter/conf/ApplicationConfig.java
@@ -2,12 +2,10 @@ package com.szatmary.peter.conf;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 /**
  * Created by peterszatmary on 07/05/2017.
